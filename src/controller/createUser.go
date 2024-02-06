3c54c55d6c40c9d78ec156d151753853a13acfe5
Mathews <mathewsrodrigues9@gmail.com>
2024-02-05 21:40:47 -0300

    retorna erro caso um campo for invalido

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,20 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/mathews-r/golang/src/controller/model/request"
 	"github.com/mathews-r/golang/src/rest_err"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestErr("Você chamou a rota errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestErr(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
