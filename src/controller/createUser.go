35add5ce9e3bb1de3ea2c6bfdea776fb979602ac
Mathews <mathewsrodrigues9@gmail.com>
2024-02-06 08:57:50 -0300

    adiciona personalização de erro ao chamar um endpoint

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,18 +1,16 @@
 package controller
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
+	"github.com/mathews-r/golang/src/configs/validation"
 	"github.com/mathews-r/golang/src/controller/model/request"
-	"github.com/mathews-r/golang/src/rest_err"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestErr(fmt.Sprintf("Some fields are incorrect"))
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
