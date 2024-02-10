b4b6f692fff19bfaadad511bf5b10d8de6ffb0f5
Mathews <mathewsrodrigues9@gmail.com>
2024-02-10 14:27:57 -0300

    criando conexão com banco de dados

---
+++ src/configs/database/mongodb/mongodb_connection.go
@@ -0,0 +1,32 @@
+package mongodb
+
+import (
+	"context"
+	"os"
+
+	"github.com/mathews-r/golang/src/configs/logger"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	DB_URL  = "DB_URL"
+	DB_NAME = "DB_NAME"
+)
+
+func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	mongodbURL := os.Getenv(DB_URL)
+	dbName := os.Getenv(DB_NAME)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodbURL))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	logger.Info("Conectado ao banco de dados com sucesso")
+	return client.Database(dbName), nil
+}
